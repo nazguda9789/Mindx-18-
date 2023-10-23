3e66b240627d94a50436cc5b218b9050b450d961
nazguda9789 <huykhoaitay2007@gmail.com>
2023-10-23 19:05:14 +0700

    ss8

---
+++ SS8/index.js
@@ -0,0 +1,26 @@
+// function run(n) {
+//     let count = 0
+//     if (1 <= n <= 1000000){
+//         if (n % 2 === 0) {
+//             count = count +1
+//             n = n / 2
+//         }
+//     }
+// }
+
+
+let key = 2 
+
+
+switch(key){
+    case 1:
+        console.log(1)
+    case 2:
+        console.log(2)
+    case 4:
+        console.log(4)
+        break;
+    default:
+        console.log(3)
+        break;
+}
